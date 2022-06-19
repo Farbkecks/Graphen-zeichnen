7d84380ab5f04c45c495e8020c326ef3c0ff07d8
Farbkecks <fabian.alich@gmx.de>
2022-06-19 20:48:18 +0200

    zwei y auf einer höhe

--- src/App.java
+++ src/App.java
@@ -1,3 +1,4 @@
+
 // import java.util.Scanner;
 // import java.util.Arrays;
 
@@ -20,20 +21,36 @@ public class App {
 
     static void print_graphen(int[][] table){
         System.out.println("/\\");
+        System.out.print("|");
         for (int y=0; y<table.length; y++){
             int[] pos = table[y];
-            System.out.print("|");
-            for (int i=0; i<pos[0];i++){
-                System.out.print(" ");
+            int steps_right = pos[0];
+            if (y>0){
+                if(pos[1]==table[y-1][1]){
+                    steps_right = steps_right - table[y-1][0];
+                }
             }
-            System.out.println("*");
+            
+            for (int i=0; i<steps_right;i++){
+                System.out.print("    ");
+            }
+            System.out.print("*");
             if (y!= table.length-1){
-                int steps = pos[1]-table[y+1][1];
-                for (int i=0; i<steps;i++){
+                int steps_down = pos[1]-table[y+1][1];
+                if(steps_down==0){
+                    continue;
+                }
+                System.out.println();
+                for (int i=1; i<steps_down;i++){
                     System.out.println("|");
                 }
             }
+        }
+        System.out.print("|");
+       for (int i=0; i<table[table.length-1][1];i++){
+            System.out.println("|");
        }
+       System.out.println();
         for (int i=0; i<table[0][0]+1; i++){
             System.out.print("---");
         }
@@ -46,9 +63,11 @@ public class App {
         {2,12}, 
         {3,5}, 
         {4,12}, 
-        {5,6}, 
-        {6,8}};
+        {5,5}, 
+        {6,8},
+        {7,2}};
         int [][] table = resort_table(table_original);
         print_graphen(table);
+
         }
 }
