e1c58e2e172e593d7f47d2e9d2c4ae1e2bfb134d
Farbkecks <fabian.alich@gmx.de>
2022-07-02 13:07:51 +0200

    args for debug

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println(args[0]);
         Graph graph = new Graph(Generator.generateBasicGraph());
         // graph.debugPrintAsList();
         graph.draw();
