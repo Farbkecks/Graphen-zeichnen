177650960edbfd951df37431e2b4e70003a9e30d
Farbkecks <fabian.alich@gmx.de>
2022-07-03 12:19:20 +0200

    add FormelPart

---
+++ src/FormalPart.java
@@ -0,0 +1,13 @@
+public class FormalPart {
+    Sign sign;
+    Operation operation;
+    double y;
+    double resault;
+
+    public FormalPart(Sign sign, Operation operation, double y){
+        this.sign = sign;
+        this.operation = operation;
+        this.y = y;
+        this.resault=0;
+    }
+    }    
--- src/Generator.java
+++ src/Generator.java
@@ -1,16 +1,32 @@
 import java.lang.Math;
 import java.util.ArrayList;
 
+enum Operation{
+    times,
+    divided,
+    power,
+    root
+}
+
+enum Sign{
+    add,
+    subtract
+}
+
 public class Generator {
     static ArrayList<Coordinate2D> generateBasicGraph(String formel){
         ArrayList<Coordinate2D> graph = new ArrayList<>();
-        for (int x= 1; x<20;x++){
+  
+        return graph;
+    } 
+}
+/*
+ *       for (int x= 1; x<20;x++){
             int y = (int) Math.round(Math.pow(Double.valueOf(x), 2.0));
             if (y > 20){
                 break;
             }
             graph.add(new Coordinate2D(x, y));
         }
-        return graph;
-    } 
-}
+ * 
+ */
