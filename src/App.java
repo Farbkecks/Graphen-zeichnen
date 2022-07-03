ba453b06a0aaca442d21431730732213fa9e91c5
Farbkecks <fabian.alich@gmx.de>
2022-07-03 11:59:41 +0200

    take formal as args

--- src/App.java
+++ src/App.java
@@ -1,7 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(args[0]);
-        Graph graph = new Graph(Generator.generateBasicGraph());
+        switch(args.length){
+            case 0: {
+                System.out.println("Bitte eine Formel angeben");
+                System.exit(0);
+            }
+            case 2: {
+                System.out.println("Nur eine Formel angeben");
+                System.exit(0);
+            }
+        }
+        String formel = args[1];
+        Graph graph = new Graph(Generator.generateBasicGraph(formel));
         // graph.debugPrintAsList();
         graph.draw();
     }
--- src/Generator.java
+++ src/Generator.java
@@ -2,7 +2,7 @@ import java.lang.Math;
 import java.util.ArrayList;
 
 public class Generator {
-    static ArrayList<Coordinate2D> generateBasicGraph(){
+    static ArrayList<Coordinate2D> generateBasicGraph(String formel){
         ArrayList<Coordinate2D> graph = new ArrayList<>();
         for (int x= 1; x<20;x++){
             int y = (int) Math.round(Math.pow(Double.valueOf(x), 2.0));
